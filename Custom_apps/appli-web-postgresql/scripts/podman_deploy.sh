017bf63e0cf4d5706446bddf29f82c8ea50abc8f
thibgrev <thibgrev@hotmail.com>
2025-03-04 11:32:35 +0100

    Change FR to ENG

--- Custom_apps/appli-web-postgresql/scripts/podman_deploy.sh
+++ Custom_apps/appli-web-postgresql/scripts/podman_deploy.sh
@@ -32,67 +32,71 @@
             API_PORT=$(grep -i 'api_port' $CONFIG_FILE | awk -F ' = ' '{print $2}')
             SWAGGER_PORT=$(grep -i 'swagger_port' $CONFIG_FILE | awk -F ' = ' '{print $2}')
 
-# Vérifier si le réseau existe
-if sudo podman network inspect "$NETWORK_NAME" > /dev/null 2>&1; then
-    echo "Le réseau $NETWORK_NAME existe déjà."
-else
-    # Créer le réseau s'il n'existe pas
-    echo "Le réseau $NETWORK_NAME n'existe pas. Création en cours..."
-    sudo podman network create "$NETWORK_NAME"
-    echo "Réseau $NETWORK_NAME créé avec succès."
-fi
+#Test of network existing
+    if sudo podman network inspect "$NETWORK_NAME" > /dev/null 2>&1; then
+        echo "Network $NETWORK_NAME exist."
+    else
+        #Network creation because it doesn t exist
+        echo "Network $NETWORK_NAME doesn t exist. Creation in progress"
+        sudo podman network create "$NETWORK_NAME"
+        echo "Network $NETWORK_NAME created"
+    fi
 
-if sudo podman volume inspect "$STORAGE_NAME" > /dev/null 2>&1; then
-    echo "Le volume $STORAGE_NAME existe déjà."
-else
-    # Créer le volume s'il n'existe pas
-    echo "Le volume $STORAGE_NAME n'existe pas. Création en cours..."
-    sudo podman volume create "$STORAGE_NAME"
-    echo "Volume $STORAGE_NAME créé avec succès."
-fi
+#Test of volume existing
+    if sudo podman volume inspect "$STORAGE_NAME" > /dev/null 2>&1; then
+        echo "Volume $STORAGE_NAME exist"
+    else
+        #Volume creation because it doesn t exist
+        echo "Volume $STORAGE_NAME doesn t exist. Creation in progress"
+        sudo podman volume create "$STORAGE_NAME"
+        echo "Volume $STORAGE_NAME created"
+    fi
 
-# Chemin du fichier de configuration CNI
-CNI_CONFIG_FILE="/etc/cni/net.d/$NETWORK_NAME.conflist"
+#CNI Conf file path
+    CNI_CONFIG_FILE="/etc/cni/net.d/$NETWORK_NAME.conflist"
 
-# Vérifier si le fichier existe
-if [ -f "$CNI_CONFIG_FILE" ]; then
-    # Modifier la version CNI à "0.4.0"
-    sudo sed -i 's/"cniVersion": ".*"/"cniVersion": "0.4.0"/' "$CNI_CONFIG_FILE"
-    echo "La version CNI a été modifiée à 0.4.0 dans $CNI_CONFIG_FILE."
-else
-    echo "Le fichier de configuration $CNI_CONFIG_FILE n'existe pas. Assurez-vous que le réseau est créé."
-fi
+#Test if file exist
+    if [ -f "$CNI_CONFIG_FILE" ]; then
+        #Modification from version CNI to "0.4.0"
+        sudo sed -i 's/"cniVersion": ".*"/"cniVersion": "0.4.0"/' "$CNI_CONFIG_FILE"
+        echo "CNI vers modified to 0.4.0 in $CNI_CONFIG_FILE."
+    else
+        echo "$CNI_CONFIG_FILE doesn t exist. Be sure network is created"
+    fi
 
-sudo podman run -d --name $BACKEND  --network appli-web-postgresql_network \
-    --network $NETWORK_NAME \
-    -e POSTGRES_DB=$DB_NAME \
-    -e POSTGRES_USER=$DB_USER \
-    -e POSTGRES_PASSWORD=$DB_PASSWORD \
-    -v $STORAGE_NAME:/bitnami/postgresql \
-    $BACKEND_IMAGE
+#Deployement of conteneur PostGreSQL
+    sudo podman run -d --name $BACKEND  --network appli-web-postgresql_network \
+        --network $NETWORK_NAME \
+        -e POSTGRES_DB=$DB_NAME \
+        -e POSTGRES_USER=$DB_USER \
+        -e POSTGRES_PASSWORD=$DB_PASSWORD \
+        -v $STORAGE_NAME:/bitnami/postgresql \
+        $BACKEND_IMAGE
 
+#Deployement of conteneur Frontend
+    sudo podman run -d --name $FRONTEND --network appli-web-postgresql_network \
+        --network $NETWORK_NAME \
+        -e DB_HOST=$BACKEND \
+        -e DB_NAME=$DB_NAME \
+        -e DB_USER=$DB_USER \
+        -e DB_PASSWORD=$DB_PASSWORD \
+        -p $FRONTEND_PORT:5000 \
+        $FRONTEND_IMAGE
+#Deployement of conteneur API
+    sudo podman run -d --name $API --network $NETWORK_NAME \
+    -p $API_PORT:8000 $API_IMAGE
 
-sudo podman run -d --name $FRONTEND --network appli-web-postgresql_network \
-    --network $NETWORK_NAME \
-    -e DB_HOST=$BACKEND \
-    -e DB_NAME=$DB_NAME \
-    -e DB_USER=$DB_USER \
-    -e DB_PASSWORD=$DB_PASSWORD \
-    -p $FRONTEND_PORT:5000 \
-    $FRONTEND_IMAGE
+#Deployement of conteneur Swagger
+    sudo podman run -d --name $SWAGGER --network $NETWORK_NAME \
+    -p $SWAGGER_PORT:5000 $SWAGGER_IMAGE
 
-sudo podman run -d --name $API --network $NETWORK_NAME \
-  -p $API_PORT:8000 $API_IMAGE
-
-sudo podman run -d --name $SWAGGER --network $NETWORK_NAME \
-  -p $SWAGGER_PORT:5000 $SWAGGER_IMAGE
-
-sudo podman run -d --name $PGADMIN --network $NETWORK_NAME \
-    -e 'PGADMIN_DEFAULT_EMAIL='$PGADMIN_EMAIL \
-    -e 'PGADMIN_DEFAULT_PASSWORD='$PGADMIN_PASSWORD \
-    -e 'PGADMIN_CONFIG_SERVER_MODE=False' \
-    -e 'PGADMIN_CONFIG_ENHANCED_COOKIE_PROTECTION=False' \
-    -e 'PGADMIN_DISABLE_POSTGRES_MASTER_PASSWORD=True' \
-    -v ../PGAdmin/config.json:/pgadmin4/servers.json \
-    -p $PGADMIN_PORT:80 \
-    $PGADMIN_IMAGE
+#Deployement of conteneur PGAdmin
+    sudo podman run -d --name $PGADMIN --network $NETWORK_NAME \
+        -e 'PGADMIN_DEFAULT_EMAIL='$PGADMIN_EMAIL \
+        -e 'PGADMIN_DEFAULT_PASSWORD='$PGADMIN_PASSWORD \
+        -e 'PGADMIN_CONFIG_SERVER_MODE=False' \
+        -e 'PGADMIN_CONFIG_ENHANCED_COOKIE_PROTECTION=False' \
+        -e 'PGADMIN_DISABLE_POSTGRES_MASTER_PASSWORD=True' \
+        -v ../PGAdmin/config.json:/pgadmin4/servers.json \
+        -p $PGADMIN_PORT:80 \
+        $PGADMIN_IMAGE
